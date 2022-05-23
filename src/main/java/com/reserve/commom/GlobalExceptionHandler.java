a53507345a5cf96311fd83b787433f6fe2cdf6f9
Hzone1110 <592732293@qq.com>
2022-05-23 12:26:20 +0800

    Changes

---
+++ src/main/java/com/reserve/commom/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.reserve.commom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.ConnectException;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = ConnectException.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> exceptionHandler(Exception e) {
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "数据库连接异常");
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
